51164b259fcaf850161ff78266f3c41f753620e7
himajinx <151967111+himajinx@users.noreply.github.com>
2023-12-27 23:11:23 +0900

    Update cooking.php

--- cooking.php
+++ cooking.php
@@ -1,60 +1,34 @@
 <?php
-$file = fopen("orders.csv", "r");
-$orders = array();
-while (($line = fgetcsv($file)) !== FALSE) {
- $tableId = $line[0];
- $orderId = $line[1];
- $quantity = $line[2];
- if (!isset($orders[$tableId])) {
-    $orders[$tableId] = array();
- }
- $orders[$tableId][] = array($orderId, $quantity);
+$file = 'table12.csv'; // CSVファイル名を設定。tables.csv={tableid:{{foodname,foodquantity}}}に書き換える必要がありそう
+$data = [];
+
+if (($handle = fopen($file, "r")) !== FALSE) {
+  while (($row = fgetcsv($handle, 1000, ",")) !== FALSE) {
+      if ($row[2] != 0) { // foodquantityが0でない場合のみデータを格納
+          $data[] = ['foodname' => $row[0], 'foodquantity' => $row[2]];
+      }
+  }
+  fclose($handle);
 }
-fclose($file);
 ?>
 
-
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
-
-<div class="container mt-5">
-<?php foreach ($orders as $tableId => $orderList): ?>
- <div class="card" style="width: 18rem;" data-bs-toggle="modal" data-bs-target="#detailsModal<?php echo $tableId; ?>">
- <div class="card-body">
-   <h5 class="card-title">テーブル<?php echo $tableId; ?>番</h5>
-   ここにそのままメニュー一覧を表示すればいい
-
- </div>
- </div>
-
- <div class="modal fade" id="detailsModal<?php echo $tableId; ?>" tabindex="-1" role="dialog" aria-labelledby="detailsModalLabel<?php echo $tableId; ?>" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="detailsModalLabel<?php echo $tableId; ?>"><?php echo $tableId; ?></h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <table class="table table-striped">
-        <?php foreach ($orderList as $order): ?>
-          <tr>
-            <th scope="row"><?php echo $order[0]; ?></th>
-            <td><?php echo $order[1]; ?></td>
-          </tr>
-        <?php endforeach; ?>
-        </table>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
- </div>
-<?php endforeach; ?>
-</div>
-
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Table Data</title>
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+</head>
+<body>
+   <div class="container">
+      <?php foreach ($data as $item): ?>
+          <div class="card mb-3">
+              <div class="card-header">Table ID</div> <!-- Table IDを表示 -->
+              <ul class="list-group list-group-flush">
+                 <li class="list-group-item"><?php echo $item['foodname']; ?></li> <!-- foodnameを表示 -->
+                 <li class="list-group-item"><?php echo $item['foodquantity']; ?></li> <!-- foodquantityを表示 -->
+              </ul>
+          </div>
+      <?php endforeach; ?>
+   </div>
+</body>
+</html>
